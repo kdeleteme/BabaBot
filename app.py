fe8bee9b266f0d3dcf5f6f9c3cf38e72b56781d4
Kent Delante <kdeleteme@tutanota.com>
2020-05-31 08:34:36 +0800

    Use a main function, it makes more sense

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ import os
 
 from bababot import Bababot
 
-if __name__ == '__main__':
-    key = os.environ['API_KEY']
-    bababot = Bababot()
+def main():
+    bababot = Bababot(joke_fetcher)
+    key = os.getenv('API_KEY')
     bababot.run(key)
+
+if __name__ == '__main__':
+    main()
