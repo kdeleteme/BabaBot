3796b7739805c191d3351f659f4fd00cc6d10210
Kent Delante <kdeleteme@tutanota.com>
2020-12-19 14:24:24 +0800

    Change app filename to bot

--- bot.py
+++ bot.py
@@ -1,11 +1,15 @@
 import os
 
+from discord import Intents
+
 from bababot.bababot import Bababot
 from bababot.jokefetcher import JokeFetcher
 
 
 def main():
-    bababot = Bababot(JokeFetcher())
+    intents = Intents.default()
+    intents.members = True
+    bababot = Bababot(JokeFetcher(), intents)
 
     key = os.getenv('API_KEY')
     bababot.run(key)
