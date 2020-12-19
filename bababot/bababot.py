f6a4f5bf12793b2f7ca871ab61c1d0e80cc29996
Kent Delante <kdeleteme@tutanota.com>
2020-12-19 20:09:31 +0800

    Add more personality to the welcome message

--- bababot/bababot.py
+++ bababot/bababot.py
@@ -21,7 +21,8 @@ class Bababot(Bot):
         channel = member.guild.system_channel
 
         if channel is not None:
-            await channel.send(f"Welcome {member.mention}!")
+            await channel.send(f"Welcome to the family {member.mention}! "
+                               "Don't disappoint me and you will be fine.")
 
     async def on_ready(self) -> None:
         print(f"Logged on as {self.user}!")
