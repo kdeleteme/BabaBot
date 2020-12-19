8226d800c59ad39786f472480343688a9f185765
Kent Delante <kdeleteme@tutanota.com>
2020-12-19 14:22:31 +0800

    Inherit Bot instead of Client for use in tandem with Cog

--- bababot/bababot.py
+++ bababot/bababot.py
@@ -1,65 +1,38 @@
-from discord import Client
+from discord import Intents, Member
+from discord.ext import commands
+from discord.ext.commands import Bot
 
 from bababot.jokefetcher import JokeFetcher
 
 
-class Bababot(Client):
+class Bababot(Bot):
     """Your Strict Asian dad bot"""
 
     joke_fetcher: JokeFetcher
 
-    def __init__(self, joke_fetcher: JokeFetcher):
-        super().__init__()
+    def __init__(self, joke_fetcher: JokeFetcher, intents: Intents):
+        super().__init__(intents = intents, command_prefix='?')
         self.joke_fetcher = joke_fetcher
 
-    async def print_commands(self, message) -> None:
-        await message.channel.send(
-            '```Politely ask Baba the following:\n\n'
+    def setup(self):
+        self.load_extension('bababot.cogs.CommandCog')
 
-            '?info             Get to know your Baba\n'
-            '!ping             Ping your Baba\n'
-            '?joke             Listen to Baba joke\n'
-            '?hot              Ask Baba to say "That\'s hot!"\n'
-            '?commands         Show this list of commands\n'
-            '?slap @username   Ask Baba to slap the person mentioned```'
-        )
+    async def on_member_join(self, member: Member):
+        channel = member.guild.system_channel
+
+        if channel is not None:
+            await channel.send(f"Welcome {member.mention}!")
 
     async def on_ready(self) -> None:
         print(f"Logged on as {self.user}!")
+        self.setup()
 
     async def on_message(self, message) -> None:
-        # print('{0.author}: {0.content}'.format(message))
-
-        if (message.author == self.user):
+        if (message.author.bot):
             return
 
-        if (message.content == '!ping'):
-            await message.channel.send('pong!')
-
-        if (message.content == '?joke'):
-            await message.channel.send(await self.joke_fetcher.fetch())
-
-        if ('fortnite' in message.content.lower()):
-            await message.channel \
-                    .send('Say Fortnite again son and your dog will be gone.')
-
-        if (message.content == '?hot'):
-            await message.channel.send('That\'s hot!')
-
-        if (message.content == '?commands'):
-            await self.print_commands(message)
-
-        if (message.content == '?info'):
-            await message.channel.send(
-                'I am your Baba. '
-                'You will listen to me. '
-                'Disappoint me and I will disown you.\n\n'
-                '*Send **!commands** in chat to see how to interact with '
-                'your Baba*\n\n'
-                'I was built by kdeleteme. You can find my brain at'
-                ' https://gitlab.com/kdeleteme/bababot.'
-            )
-
+        await self.process_commands(message)
+        
         if 'i\'m' in message.content.lower() \
            or 'im' in message.content.lower() \
            or 'i am' in message.content.lower():
@@ -84,8 +57,6 @@ class Bababot(Client):
 
                 noun = ' '.join(noun_array)
                 await message.channel.send(f'Haro {noun}, I\'m Baba.')
-                    
-        if ('?slap' in message.content.lower()):
-            await message.channel \
-                         .send('I won\'t allow you children to resort to'
-                               ' violence. You need to behave!')
+
+        if ('fortnite' in message.content.lower()):
+            await message.channel.send('Say Fortnite again son and your dog will be gone.')
